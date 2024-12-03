8e04bb903e582a78a141debaea8193007484d5ed
myle2136 <lemy2136@gmail.com>
2024-12-03 10:38:51 +0700

    Add files via upload

---
+++ lan1_220228_da24tta_110124124.cpp
@@ -0,0 +1,31 @@
+/*
+	MSSV:110124124
+	HoTen:Le Thi Diem My 
+	Lop:DA24TTA
+*/
+#include<stdio.h>
+#include<math.h>
+#define SIZE 7
+ main ()
+{
+	int a[SIZE],i;
+	/*Nhap vao so gio hoc cua tung ngay*/
+	for(i=0;i<SIZE;i++)	
+	{
+		printf("\nNhap a[%f]",i);
+		scanf ("%f",&a[i]);		
+	}
+	if (a[i]>0&a[i]<15 )
+		printf ("\nSo gio hoc trong tuan la:");
+		for(i=0;i<SIZE;i++);
+		{
+			printf ("%6f",a[i]);			
+		}
+	else 
+		printf ("\nSo gio nhap khong hop le:");
+		return 0;	
+		
+}	
+
+
+	
